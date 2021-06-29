c7e46827c38d160b86c6581ab5fc4db829c96222
josevaltier <84319890+josevaltier@users.noreply.github.com>
2021-06-29 19:51:10 +0200

    Updated date and time

--- src/app.js
+++ src/app.js
@@ -1,5 +1,44 @@
+function currentDate(timestamp) {
+  let now = new Date(timestamp);
+  let date = now.getDate();
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[now.getMonth()];
+  return `${day}, ${month} ${date} - ${hours}:${minutes}`;
+}
+
 function showTemperature(response) {
-  console.log(response);
   let mainTemp = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
@@ -7,6 +46,7 @@ function showTemperature(response) {
   let windValue = document.querySelector("#windSpeed");
   let minimumTemperature = document.querySelector("#min-temp");
   let maximumTemperature = document.querySelector("#max-temp");
+  let dateElement = document.querySelector("#date");
 
   mainTemp.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -15,10 +55,11 @@ function showTemperature(response) {
   windValue.innerHTML = Math.round(response.data.wind.speed);
   minimumTemperature.innerHTML = Math.round(response.data.main.temp_min);
   maximumTemperature.innerHTML = Math.round(response.data.main.temp_max);
+  dateElement.innerHTML = currentDate(response.data.dt * 1000);
 }
 
 let apiKey = "ff39a1560b2a6b58581393d9865ab25f";
 
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Barcelona&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(showTemperature);
+axios.get(apiUrl).then(showTemperature).then(currentDate);
